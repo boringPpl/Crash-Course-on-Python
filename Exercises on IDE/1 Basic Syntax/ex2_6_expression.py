d5608b047cdde3fb9179e2648cdc5d5eb086f839
daphnee97 <44963656+daphnee97@users.noreply.github.com>
2020-10-05 15:24:29 +0700

    Add files via upload

---
+++ Exercises on IDE/1 Basic Syntax/ex2_6_expression.py
@@ -0,0 +1,10 @@
+''' Question 2.6:
+Keeping in mind there are 86400 seconds per day, 
+write a program that calculates how many seconds there are in a year, 
+if a year is 365 days.Print the result on the screen.
+
+Note: Your result should be in the format of just a number, not a sentence.
+'''
+
+
+
