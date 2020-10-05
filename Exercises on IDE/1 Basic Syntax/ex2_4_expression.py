fd684ef98e68df56c60fddd18f98eb2e5d81f8ce
daphnee97 <44963656+daphnee97@users.noreply.github.com>
2020-10-05 15:29:35 +0700

    Delete ex2_4_expression.py

--- Exercises on IDE/1 Basic Syntax/ex2_4_expression.py
+++
@@ -1,7 +0,0 @@
-''' Question 2.4:
-This code is supposed to display "5 + 3 = 8" on the screen, but there is an error. 
-Find the error in the code and fix it, so that the output is correct.
-'''
-
-print("2 + 2 = " + (2 + 2))
-
