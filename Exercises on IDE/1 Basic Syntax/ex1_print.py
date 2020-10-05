9ab9dd07bf78be80c33256242c48af7fc85bd4b0
daphnee97 <44963656+daphnee97@users.noreply.github.com>
2020-10-05 15:22:59 +0700

    Add files via upload

---
+++ Exercises on IDE/1 Basic Syntax/ex1_print.py
@@ -0,0 +1,16 @@
+'''Question 1.1:
+Fill in the __ to print output the text on the screen
+'''
+
+# Output: Hello World
+print(__)
+
+# Output: Hello [your name]
+print(__)
+
+# Output: I love interactive videos!
+print(__)
+
+# Output: How to print "Hello World"?
+print(__)
+
