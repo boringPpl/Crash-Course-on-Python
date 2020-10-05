b032901e1fe2c18f7023f8af76b3cabed9651e2e
daphnee97 <44963656+daphnee97@users.noreply.github.com>
2020-10-05 12:19:50 +0700

    Add files via upload

---
+++ basic_syntax.py
@@ -0,0 +1,11 @@
+# print "Hello World"
+print("Hello World")
+
+# print "Hello [your name]"
+
+# print "I am learning Python"
+
+# print "I love interactive videos"
+
+# print "[anything you want]"
+
