1779ff0ef87b5138c38d50db959bd05a24bdb8a4
daphnee97 <44963656+daphnee97@users.noreply.github.com>
2020-10-06 14:53:45 +0700

    Add files via upload

--- Exercises on IDE/3 Function/ex3_2_function.py
+++ Exercises on IDE/3 Function/ex3_2_function.py
@@ -1,8 +1,8 @@
 ''' Question 3.2:
 Write a find_lucky_number function. Your lucky number is equal to the length of your name times 8.
-The function receives a name as parameter. And return a message. 
-Fill in the blanks to complete the code to make it work.
-
+The function receives a name as parameter. 
+And return a message that mentioned your name, and your lucky number. 
+Fill in the blanks to complete the code to make it work
 
 Note: Instead of printing the message, we will return the message. 
 This way, the calling line can print the message, 
